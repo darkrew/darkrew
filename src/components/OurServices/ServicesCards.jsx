0e90c714e3eef9d12bb54a097f5008f966672c26
Muhammad Tayyab <hackert960@gmail.com>
2024-05-25 15:12:43 +0500

    Fix text Size

--- src/components/OurServices/ServicesCards.jsx
+++ src/components/OurServices/ServicesCards.jsx
@@ -8,7 +8,7 @@ function ServicesCards({ svg, title, description }) {
           {svg}
         </div>
       </div>
-      <h3 className="mb-2 text-md font-bold text-[var(--main-color)] uppercase headings text-center md:text-start lg:text-lg xl:text-xl">
+      <h3 className="mb-2 text-md font-bold text-[var(--main-color)] uppercase headings  text-justify md:text-start lg:text-lg xl:text-xl">
         {title}
       </h3>
       <p className="text-[var(--text-color)] subHeadings xl:text-lg text-center lg:text-start">
