a81cb814a86d0c998d4f724635ef9b9189cbaab0
Muhammad Tayyab <hackert960@gmail.com>
2024-06-02 16:22:16 +0500

    fix the logo redirect

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,13 +35,17 @@ function Navbar() {
     <>
       <nav className="bg-white fixed w-full z-20 top-0 start-0 border-b border-gray-200 h-[5rem]">
         <div className="px-3 flex flex-wrap items-center justify-between mx-auto h-full">
-          <a href="https://flowbite.com/" className="flex items-center h-full">
+          <NavLink
+            to={"/"}
+            onClick={GoHome}
+            className="flex items-center h-full"
+          >
             <img
               src="/Images/Logo/DK-01.png"
               className="max-h-36"
               alt="Darkrew Logo"
             />
-          </a>
+          </NavLink>
           <div className="flex md:order-2 space-x-3 md:space-x-0">
             <button
               type="button"
